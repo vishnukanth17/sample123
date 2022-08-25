eb56d72557cc254c688330f495ebbb2d4c90bda1
ShriyaRedyy <aletishriyareddy10gmail.com>
2022-08-25 13:51:23 +0530

    first commit

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState, useEffect } from 'react'
+import Skeleton from 'react-loading-skeleton';
 
 function Product() {
     const [data, setData] = useState([]);
@@ -29,35 +30,53 @@ function Product() {
 
     }, []);
 
+    
     const Loading = () => {
         return (
             <>
-                Loading......
+                <div className="col-md-3">
+                    <Skeleton height={350}/>
+                </div>
+                <div className="col-md-3">
+                    <Skeleton height={350}/>
+                </div>
+                <div className="col-md-3">
+                    <Skeleton height={350}/>
+                </div>
+                <div className="col-md-3">
+                    <Skeleton height={350}/>
+                </div>
             </>
         )
     }
+
+    const filterProduct = (cat) => {
+        const updatedList =data.filter((x)=>x.category === cat);
+        setFilter(updatedList);
+    }
+  
     const ShowProducts = () => {
 
         return (
             <>
                 <div className="buttons d-flex justify-content-center mb-5 pb-5">
-                    <button className="btn btn-ouline-dark">All</button>
-                    <button className="btn btn-ouline-dark me-2">Men's Clothing</button>
-                    <button className="btn btn-ouline-dark me-2">Women's Clothing</button>
-                    <button className="btn btn-ouline-dark me-2">Jewlery </button>
-                    <button className="btn btn-ouline-dark me-2">Electronic   </button>
+                    <button className="btn btn-ouline-dark shadow" onClick={() => setFilter(data)}>All</button>
+                    <button className="btn btn-ouline-dark me-2 shadow" onClick={() => filterProduct("men's clothing")}>Men's Clothing</button>
+                    <button className="btn btn-ouline-dark me-2 shadow" onClick={() => filterProduct("women's clothing")}>Women's Clothing</button>
+                    <button className="btn btn-ouline-dark me-2 shadow" onClick={() => filterProduct("jewelery")}>Jewlery </button>
+                    <button className="btn btn-ouline-dark me-2 shadow" onClick={() => filterProduct("electronics")}>Electronic   </button>
                 </div>
                 <div className='row justify-center'>
 
                     {filter.map((product) => {
                         return (
-                            <div className="col-md-3">
+                            <div className="col-md-3 mb-4">
                                 <div class="card h-100 text-center p-4 " key={product.id} >
-                                    <img src={product.image} class="card-img-top" alt={product.title} />
+                                    <img src={product.image} class="card-img-top" alt={product.title} height="250px"/>
                                     <div class="card-body">
-                                        <h5 class="card-title">{product.title}</h5>
-                                        <p class="card-text">${product.price}</p>
-                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                        <h5 class="card-title mb-0">{product.title.substring(0,12)}...</h5>
+                                        <p class="card-text lead fw-bold">${product.price}</p>
+                                        <a href="#" class="btn btn-outline-dark">Buy Now</a>
                                     </div>
                                 </div>
 
